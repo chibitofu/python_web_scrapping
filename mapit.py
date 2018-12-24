0579e9624c67039119f18768e87f2e9c2b69c2b0
chibitofu <erinmoony@gmail.com>
2018-12-24 11:04:36 -0800

    Takes input from command line.

--- mapit.py
+++ mapit.py
@@ -1,2 +1,9 @@
-import webbrowser
-webbrowser.open('http://inventwithpython.com/')
+#! python3
+# mapIt.py - Launches a map in the browser using an addesss from the command line or clipboard.
+
+import webbrowser, sys
+if len(sys.argv) > 1:
+    # Get address from command line.
+    address = ' '.join(sys.argv[1:])
+
+# TODO: GEt address from clipboard
