be61f0fdc0d6962231898427b11c8fa6a448cdcc
chibitofu <erinmoony@gmail.com>
2018-12-24 11:15:12 -0800

    Take input from clipboard now.

--- mapit.py
+++ mapit.py
@@ -1,9 +1,12 @@
 #! python3
 # mapIt.py - Launches a map in the browser using an addesss from the command line or clipboard.
 
-import webbrowser, sys
+import webbrowser, sys, pyperclip
 if len(sys.argv) > 1:
     # Get address from command line.
     address = ' '.join(sys.argv[1:])
+else:
+    # Get address from clipboard.
+    address = pyperclip.paste()
 
-# TODO: GEt address from clipboard
+webbrowser.open('https://www.google.com/maps/place/' + address)
